77b54f4c7fa2c03155bfa9e2534d4a5225a1fbfe
dhruvmeghani <cdmi.dhruvmeghani@gmail.com>
2024-02-08 12:10:34 +0530

    chage

--- app.js
+++ app.js
@@ -24,9 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/',userRouter);
-app.use('/',categoryRouter);
-app.use('/',productRouter);
+app.use('/user',userRouter);
+app.use('/category',categoryRouter);
+app.use('/product',productRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
