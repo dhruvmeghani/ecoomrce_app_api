15c17148f0a725470f8d9a6e92889c2a7ef3c76b
dhruvmeghani <cdmi.dhruvmeghani@gmail.com>
2024-02-08 12:11:33 +0530

    change

--- routes/category.js
+++ routes/category.js
@@ -6,6 +6,6 @@ var category = require("../controller/category_controller");
 router.post('/c_insert',category.insert)
 router.get('/c_delete/:id',category.delete)
 router.get('/c_update/:id',category.update)
-router.post('/c',category.see_data)
+router.get('/c',category.see_data)
 
 module.exports = router;
