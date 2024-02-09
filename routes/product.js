c15c1ec76d2c6b9c73f253a78dfd6faf9cbda495
dhruvmeghani <cdmi.dhruvmeghani@gmail.com>
2024-02-09 10:38:21 +0530

    change new

--- routes/product.js
+++ routes/product.js
@@ -6,7 +6,7 @@ var product = require("../controller/product_controller");
 router.post('/p_insert',product.insert)
 router.post('/p_delete/:id',product.delete)
 router.post('/p_update/:id',product.update)
-router.post('/p',product.see_data)
+router.get('/p',product.see_data)
 
 
 module.exports = router;
