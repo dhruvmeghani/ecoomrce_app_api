850aee88be44aefe7639a5d4c0239000cd3d99e1
dhruvmeghani <cdmi.dhruvmeghani@gmail.com>
2024-02-10 11:28:57 +0530

    new changeupdate

--- routes/product.js
+++ routes/product.js
@@ -4,7 +4,7 @@ var router = express.Router();
 var product = require("../controller/product_controller");
 
 router.post('/p_insert',product.insert)
-router.post('/p_delete/:id',product.delete)
+router.get('/p_delete/:id',product.delete)
 router.post('/p_update/:id',product.update)
 router.get('/p',product.see_data)
 
